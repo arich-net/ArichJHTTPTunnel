2b366570d7ad5df1bfc299884e1b2afbf2f07ea1
Vasquez Morales <vasquez@scbxa02.ecb01.ecb.de>
2016-08-25 17:06:58 +0200

    Making bash scripts more generic

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-JAVA_HOME=/opt/java/jdk/x64/jdk
-JHTTPTUNNEL_HOME=/home/arich/Documents/ECLIPSE/Arich_Java_Projects/ArichJHTTPTunnel
-M2_REPO=/home/arich/.m2/repository
+if [ "$JAVA_HOME" != "" ]; then
+   JAVA_HOME="$JAVA_HOME"
+else
+   JAVA_HOME=/opt/java/jdk/x64/jdk
+fi
+
+if [ "$JHTTPTUNNEL_HOME" != "" ]; then
+   JHTTPTUNNEL_HOME="$JHTTPTUNNEL_HOME"
+else
+   JHTTPTUNNEL_HOME=/home/arich/Documents/ECLIPSE/Arich_Java_Projects/ArichJHTTPTunnel
+fi
+
+M2_REPO=~/.m2/repository
 M2_LOG4J=$M2_REPO/log4j/log4j/1.2.17/log4j-1.2.17.jar
 M2_JUNIT=$M2_REPO/junit/junit/3.8.1/junit-3.8.1.jar
 CLASSPATH=.:$JHTTPTUNNEL_HOME/target/ArichJHTTPTunnel-1.0-0.jar:$M2_LOG4J:$M2_JUNIT
@@ -14,15 +24,15 @@ LOCAL_PORT=22222
 #PROXY_PORT=3128
 
 if [ "$PROXY_HOST" != "" ]; then
-	$JAVA_HOME/bin/java -Dlport=$LOCAL_PORT \
-						-Dproxy=$PROXY_HOST:$PROXY_PORT \
-						-Dlog4j.configuration=log4j-console.xml \
-						-cp $CLASSPATH \
-						com.arichnet.jhttptunnel.JHttpTunnelClient $JHTTP_SERVER_HOST:$JHTTP_SERVER_PORT
+   $JAVA_HOME/bin/java -Dlport=$LOCAL_PORT \
+                       -Dproxy=$PROXY_HOST:$PROXY_PORT \
+                       -Dlog4j.configuration=log4j-console.xml \
+                       -cp $CLASSPATH \
+                       com.arichnet.jhttptunnel.JHttpTunnelClient $JHTTP_SERVER_HOST:$JHTTP_SERVER_PORT
 else
-	$JAVA_HOME/bin/java -Dlport=$LOCAL_PORT \
-						-Dlog4j.configuration=log4j-console.xml \
-						-cp $CLASSPATH \
-						com.arichnet.jhttptunnel.JHttpTunnelClient $JHTTP_SERVER_HOST:$JHTTP_SERVER_PORT
+   $JAVA_HOME/bin/java -Dlport=$LOCAL_PORT \
+                       -Dlog4j.configuration=log4j-console.xml \
+                       -cp $CLASSPATH \
+                       com.arichnet.jhttptunnel.JHttpTunnelClient $JHTTP_SERVER_HOST:$JHTTP_SERVER_PORT
 fi
 
--- scripts/server.sh
+++ scripts/server.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-JAVA_HOME=/opt/java/jdk/x64/jdk
-JHTTPTUNNEL_HOME=/home/arich/Documents/ECLIPSE/Arich_Java_Projects/ArichJHTTPTunnel
-M2_REPO=/home/arich/.m2/repository
+if [ "$JAVA_HOME" != "" ]; then
+   JAVA_HOME="$JAVA_HOME"
+else
+   JAVA_HOME=/opt/java/jdk/x64/jdk
+fi
+
+if [ "$JHTTPTUNNEL_HOME" != "" ]; then
+   JHTTPTUNNEL_HOME="$JHTTPTUNNEL_HOME"
+else
+   JHTTPTUNNEL_HOME=/home/arich/Documents/ECLIPSE/Arich_Java_Projects/ArichJHTTPTunnel
+fi
+
+M2_REPO=~/.m2/repository
 M2_LOG4J=$M2_REPO/log4j/log4j/1.2.17/log4j-1.2.17.jar
 M2_JUNIT=$M2_REPO/junit/junit/3.8.1/junit-3.8.1.jar
 CLASSPATH=.:$JHTTPTUNNEL_HOME/target/ArichJHTTPTunnel-1.0-0.jar:$M2_LOG4J:$M2_JUNIT
@@ -11,7 +21,7 @@ FORWARD_PORT=60822
 LISTEN_PORT=8888
 
 $JAVA_HOME/bin/java -Dforward=$FORWARD_HOST:$FORWARD_PORT \
-					-Dlog4j.configuration=log4j-console.xml \
-					-cp $CLASSPATH \
-					com.arichnet.jhttptunnel.JHttpTunnelServer $LISTEN_PORT
+                    -Dlog4j.configuration=log4j-console.xml \
+                    -cp $CLASSPATH \
+                    com.arichnet.jhttptunnel.JHttpTunnelServer $LISTEN_PORT
 
